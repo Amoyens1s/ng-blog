9b41ea3b5426df775622a33891ee8c37250dc311
Amoyensis <amoyensis@outlook.com>
2021-07-20 19:08:13 +0800

    🤔 获取用户数据不该给密码

--- src/user/dto/user-detail.dto.ts
+++ src/user/dto/user-detail.dto.ts
@@ -1,20 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class CreateUserDto {
+export class UserDetailDto {
   @ApiProperty({
     description: '用户名，唯一标识，可用于登录，可修改',
     example: 'Amoyensis',
     type: 'string',
   })
   readonly username: string;
-
-  @ApiProperty({
-    description:
-      '密码，不小于8位，允许数字和大小写字母，可修改，储存的为加密后字符串',
-    example: '0845A5972CD9AD4A46BAD66F1253581F',
-    type: 'string',
-  })
-  readonly password: string;
 
   @ApiProperty({
     description: '绑定邮箱，唯一标识，可用于登录，可修改',
@@ -28,7 +20,7 @@ export class CreateUserDto {
     example: '00000000000',
     type: 'number',
   })
-  readonly phone?: number;
+  readonly phone: number;
 
   @ApiProperty({
     description: '账号创建日期，不可修改',
